54fb89b95f5bc8786a03f95cc7122c4ab1736112
DESKTOP-LKCNPUO\kelli <kelliemcnaughton@gmail.com>
2017-11-26 19:41:57 +1000

    Added final styles

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-
-
 $(document).ready(function () {
     
     $("#createProfBtn").click(function(e) {
@@ -14,57 +12,34 @@ $(document).ready(function () {
 function CreateProfile()
 {
     //get users input and put in js object
+    nme = $("#name").val(),
+    pos = $("#position").val(),
+    email = $("#email").val(),
+    bio = $("#biography").val(),
      
-    
-        nme = $("#name").val(),
-        pos = $("#position").val(),
-        email = $("#email").val(),
-        bio = $("#biography").val(),
-        
-    
-    
-    //var div = $('<div class="bgSquareDisplay"><div class="col-md-3" id="left"></div><div class="col-md-9" id="right"></div></div>');
-    //$("#display").append(div).trigger("create");
-    
-    //var nameHeading = $('<h2>'+nme+'</h2'>);
-    //$("#right").append(nameHeading).trigger("create");
-    
+    //dynamically add user input to the display card
     $("#profileName").append(nme).trigger("create");
     $("#profilePosition").append(pos).trigger("create");
     $("#profileEmail").append(email).trigger("create");
     $("#profileBio").append(bio).trigger("create");
     
     $(".bgSquareDisplay").show();
-    
-        
-    //create json object from js obj
-    //var jsonObj = JSON.stringify(user);
-    
-    //send json obj to php function
-    /*$.ajax ({
-        type: "POST",
-        data: jsonObj,
-        url: "saveJson.php",
-        success: function(data) {
-            console.log("done")
-        },
-        error: function(){
-            console.log("not done")
-        }
-    })*/
-    
-    
+   
 }
 
-function readURL(input) {
-            if (input.files && input.files[0]) {
-                var reader = new FileReader();
-
-                reader.onload = function (e) {
-                    $('#profilePic')
-                        .attr('src', e.target.result);
-                };
-
-                reader.readAsDataURL(input.files[0]);
-            }
-        }
+//upload pic from user directory and display on card
+function readURL(input) 
+{
+    if (input.files && input.files[0]) 
+    {
+        var reader = new FileReader();
+        reader.onload = function (e) 
+        {
+            $('#profilePic').attr('src', e.target.result);        
+                        
+        };
+        
+        reader.readAsDataURL(input.files[0]);            
+    }
+            
+}
